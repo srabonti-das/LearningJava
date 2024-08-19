392a0a786d43146fd2644026eb0c69521267b1c5
Srabonti Das <159779293+srabonti-das@users.noreply.github.com>
2024-08-19 23:47:15 +0600

    test

---
+++ src/Basic/Boolean.java
@@ -0,0 +1,26 @@
+package Basic;
+
+public class Boolean {
+	public static void main(String[] args) {
+		
+		int myAge = 25;
+		int votingAge = 19;
+		System.out.println(myAge >= votingAge);
+		
+		
+		int myAgee = 25;
+		int votingAgee = 28;
+		
+		if (myAgee >= votingAgee) {
+			System.out.println("Old enough to vote");
+			
+		} else {
+			System.out.println("Not old enough to vote");
+		}
+		
+		
+		
+	}
+	
+
+}
